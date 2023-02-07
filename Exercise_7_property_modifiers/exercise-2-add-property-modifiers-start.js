9a1290c0ffc9c2c4fb1f76240d7f2409d83d42a4
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-02-07 14:42:49 +0000

    added property modifiers to interface and type alias

---
+++ Exercise_7_property_modifiers/exercise-2-add-property-modifiers-start.js
@@ -0,0 +1,26 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+var countryA = {
+    name: "India",
+    code: "IN",
+    population: 1352642280
+};
+var countryB = {
+    name: "Italy",
+    code: "IT"
+};
+var countryC = {
+    name: "Spain",
+    code: "ES",
+    population: 47450795
+};
+var currencyData = {
+    name: "Euro",
+    code: "EUR",
+    symbol: "€"
+};
+//currencyData.name = "Gold"; --> name is now a read-only property and can't be changed.
+currencyData.code = "GGG";
