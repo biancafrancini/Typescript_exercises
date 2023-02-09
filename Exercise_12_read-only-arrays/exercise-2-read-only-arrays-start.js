88580d908ef5a5ffd1d61f1d2433541e57a3205b
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-02-09 07:59:41 +0000

    modified exercise to stop arrays from being modified

--- Exercise_12_read-only-arrays/exercise-2-read-only-arrays-start.js
+++ Exercise_12_read-only-arrays/exercise-2-read-only-arrays-start.js
@@ -1,29 +1,19 @@
+"use strict";
 /**
  * START: Follow the instructions below.
  */
-
+exports.__esModule = true;
 // Prevent the array from being modified.
 // Use the generic interface type `ReadonlyArray`: ReadonlyArray<type>
 // This will cause a type error. Remove the code that now has a type error.
-
-const languages : ReadonlyArray<string>= ["Mongolian", "French", "Basque", "Thai"];
-
-/*languages[2] = "Hausa"; --> from now on, will not be possible change or add or remove values from this array anymore, 
+var languages = ["Mongolian", "French", "Basque", "Thai"];
+/*languages[2] = "Hausa"; --> from now on, will not be possible change or add or remove values from this array anymore,
                               because now it is a read-only type array */
-
 console.log(languages);
-
 // Prevent the array from being modified.
 // Use the readonly modifier and the array literal type syntax: readonly type[]
 // This will cause a type error. Remove the code that now has a type error.
-
-const currencies : readonly string[] = ["NZD", "THB", "NGN", "EUR", "COP", "INR"];
-
-/* currencies.reverse(); --> from now on, will not be possible change,add or remove values from the array, 
+var currencies = ["NZD", "THB", "NGN", "EUR", "COP", "INR"];
+/* currencies.reverse(); --> from now on, will not be possible change,add or remove values from the array,
                              because now it is a read-only type array */
-
 console.log(currencies);
-
-// ----
-
-export {};
