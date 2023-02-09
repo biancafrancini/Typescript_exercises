7b89d481ab7ee2e42544c025108ee52d44d7b232
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-02-09 09:42:57 +0000

    initialized Exercise_13, added tuple types and readonly tuple types

---
+++ Exercise_13_tuple_types/README.md
@@ -0,0 +1,3 @@
+# Exercise_13
+
+Define tuple types for the tuples in exercise-3-tuple-types-start.ts.
---
+++ Exercise_13_tuple_types/exercise-3-tuple-types-start.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+// Add a tuple type annotation for this tuple.
+var countryPopulation = ["China", 1412600000];
+var countryCurrency = ["Italy", "Euro", "EUR"];
+var countryLanguage = ["Greece", "Greek"];
+//countryLanguage.reverse(); --> this adjustment will no loger be acceptable because "contryLanguage" has got now a readonly type.
+console.log(countryLanguage);
