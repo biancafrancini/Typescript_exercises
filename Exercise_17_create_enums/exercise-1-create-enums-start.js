167a9c2ba633366fed342dc7bfbfb4e9ac193517
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-02-10 15:40:48 +0000

    added numeric and string enums

--- Exercise_17_create_enums/exercise-1-create-enums-start.js
+++ Exercise_17_create_enums/exercise-1-create-enums-start.js
@@ -1,60 +1,47 @@
+"use strict";
 /**
  * START: Follow the instructions below.
  */
-
+exports.__esModule = true;
 // The objects in the `countries` array use strings as the values of the
 // `currency` properties.
 // Complete this numeric enum so that it can be used instead of strings.
 // Hint: Use the string values in the `currency` properties as the enum members.
-
-enum Currency {
-    Euro,
-    Dollar,
-
-}
-
-// Update the type for the `currency` property in this interface to use
-// the `Currency` enum.
-
-interface Country {
-    name: string;
-    currency: Currency;
-}
-
+var Currency;
+(function (Currency) {
+    Currency[Currency["Euro"] = 0] = "Euro";
+    Currency[Currency["Dollar"] = 1] = "Dollar";
+})(Currency || (Currency = {}));
 // Replace the string values for the `currency` properties below with
-
-const countries: Country[] = [
+var countries = [
     {
         name: "France",
-        currency: Currency.Euro,
+        currency: Currency.Euro
     },
     {
         name: "United States of America",
-        currency: Currency.Dollar,
+        currency: Currency.Dollar
     },
     {
         name: "Italy",
-        currency: Currency.Euro,
+        currency: Currency.Euro
     },
     {
         name: "New Zealand",
-        currency: Currency.Dollar,
+        currency: Currency.Dollar
     },
 ];
-
 // Create a string enum named `LanguageStatus`. Use it to replace the
 // string values for the `status` properties in the objects below.
-
-enum LanguageStatus{
-    StatusFirst = "primary",
-    StatusSecond = "secondary"
-}
-
-const countryLanguages = [
+var LanguageStatus;
+(function (LanguageStatus) {
+    LanguageStatus["StatusFirst"] = "primary";
+    LanguageStatus["StatusSecond"] = "secondary";
+})(LanguageStatus || (LanguageStatus = {}));
+var countryLanguages = [
     { language: "Spanish", status: LanguageStatus.StatusFirst },
     { language: "English", status: LanguageStatus.StatusSecond },
 ];
 
-// ----
-
-export {};
+/* console.log(countryLanguages);
+console.log(countries);*/
