b4733b7a18243e985d6a4828d504cca30e87f0b1
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-02-10 16:50:26 +0000

    compiled JS

---
+++ Exercise_19_create_intersection_types/exercise-2-create-intersection-types-start.js
@@ -0,0 +1,17 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+var countryA = {
+    name: "Greece",
+    code: "GR",
+    languages: ["Greek"]
+};
+var countryB = {
+    name: "China",
+    code: "CN",
+    population: 1412600000
+};
+
+//console.log(countryA, countryB);
